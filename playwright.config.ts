af1de08a1c200d9283c818cb76f6968bd961686c
Kumar <kumarpal@spincitysolutions.com>
2026-09-16 18:20:26 +0100

    fix: relocate junit output to auto-generated playwright folder

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'never' }], ['json', { outputFile: 'jsonReports/jsonReport.json' }], ['dot']],
+  reporter: [['html', { open: 'never' }], ['json', { outputFile: 'jsonReports/jsonReport.json' }], ['dot'],['junit', { outputFile: 'playwright-report/junitReport.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     // baseURL: "https://ecommerce-playground.lambdatest.io/index.php?",
